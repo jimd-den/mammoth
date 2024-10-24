efec4a397f5dc77ca1be24aba3539bdda172d25e
jimd <james_d_davis@outlook.com>
2024-10-23 21:54:39 -0600

    presenter?

---
+++ src/frameworks_drivers/presenters/create_workflow_presenter.rs
@@ -0,0 +1,29 @@
+use crate::frameworks_drivers::controllers::workflow_builder::WorkflowBuilder;
+
+pub struct CreateWorkflowPresenter {
+    controller: WorkflowBuilder,
+}
+
+pub struct CreateWorkflowViewModel {
+    name: String,
+    date: String,
+}
+
+impl CreateWorkflowPresenter {
+    pub fn validate_input(
+        create_workflow_viewmodel: CreateWorkflowViewModel,
+    ) -> Result<CreateWorkflowViewModel, String> {
+        //do we even need to validate input?
+        Ok(create_workflow_viewmodel)
+    }
+
+    pub fn call(&mut self, name: String, date: String) -> Result<(), String> {
+        let workflow = CreateWorkflowViewModel { name, date };
+        let validated_workflow = Self::validate_input(workflow);
+
+        match validated_workflow {
+            Ok(workflow) => self.controller.create_new(workflow.name, workflow.date),
+            _ => return Err(String::from("Error")),
+        }
+    }
+}
--- src/frameworks_drivers/presenters/mod.rs
+++ src/frameworks_drivers/presenters/mod.rs
@@ -1,1 +1,1 @@
-
+pub mod create_workflow_presenter;
